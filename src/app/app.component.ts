6f0e03a0bdbf7c95ed068116952ff9a0be92f2b3
Saurav Srivastav <46235772+SauravSrivastav22@users.noreply.github.com>
2021-07-22 12:38:31 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -500,11 +500,11 @@ export class AppComponent {
   ]
 
   a(): void {
-    /* pass here the table id */
-    let element = document.getElementById('excel-table');
+    
+    let element = document.getElementById('table');
     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
 
-    /* generate workbook and add the worksheet */
+    
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
